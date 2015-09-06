62304008673ef3961e269609f966ee59caa5c5e3
KoynovStas <skojnov@yandex.ru>
2015-09-06 11:43:11 +0300

    add enum GPIO_Error and enum GPIO_Direction

--- linux_gpio.h
+++ linux_gpio.h
@@ -11,6 +11,23 @@ class Linux_GPIO
     public:
 
 
+        enum GPIO_Error{
+
+            ERROR_DEV_NOT_OPEN,
+            ERROR_CANT_OPEN_DEV,
+            ERROR_CANT_OPEN_EXPORT,
+            ERROR_CANT_OPEN_DIRECTION,
+            ERROR_CANT_READ,
+            ERROR_CANT_WRITE
+        };
+
+
+        enum GPIO_Direction{
+
+            GPIO_IN,
+            GPIO_OUT
+        };
+
 
          Linux_GPIO();
         ~Linux_GPIO();
